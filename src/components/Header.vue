5186ff693a792e396f4a7daabdef8b9f0ab03854
misaki_kawano <m.kawano@eisansyste.jp>
2023-05-21 12:39:35 +0900

    add: Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "App",
+  props: {
+    title: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* scoped measn this style applies to only this component */
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
